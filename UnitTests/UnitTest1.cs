a41cdbe7719fd29a9922e12591a5ba72e1017084
Steven Olufemi-OluKanni <Steven.Olufemi-OluKanni@Harrogate.Jet2.com>
2018-05-08 16:52:22 +0100

    added a test to check modal for departure appears

--- UnitTests/UnitTest1.cs
+++ UnitTests/UnitTest1.cs
@@ -5,12 +5,23 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NUnit.Framework;
 using Selenium_C_Sharp;
 using System.Linq;
+using OpenQA.Selenium.Chrome;
 
 namespace UnitTests
 {
 
     class PageObjectModel
     {
+        public static void Launch_Page()
+        { 
+            Program.driver.Navigate().GoToUrl("https://www.jet2holidays.com/");
+        }
+
+        public static void ClickPopUp()
+        {
+            IReadOnlyCollection<IWebElement> element = Program.driver.FindElements(By.ClassName("j017-close-lightbox"));
+            element.ElementAt(0).Click();
+        }
 
 
         public static void Click_List_Departure()
@@ -19,8 +30,23 @@ namespace UnitTests
             element.ElementAt(0).Click();
         }
 
-    }
+        public static bool Check_Choose_departure_modal()
+        {
+            bool holder = false;
+            IReadOnlyCollection<IWebElement> elements = Program.driver.FindElements(By.XPath("//*[@class='modal-box__heading']"));
+            foreach (IWebElement element in elements)
+            {
+                if (element.Text.Contains("Choose departure"))
 
+                {
+                    holder = element.Displayed;
+                }
+            }
+            return holder;
+        }
+
+
+    }
 
     [TestFixture]
     public class MyFirstTest
@@ -28,7 +54,22 @@ namespace UnitTests
         [Test]
         public void TestDropDown()
         {
-            
+            PageObjectModel.Launch_Page();
+            PageObjectModel.ClickPopUp();
+            PageObjectModel.Click_List_Departure();
+        }
+    }
+
+
+
+    [TestFixture]
+    public class MySecondTest
+    {
+        [Test]
+        public void TestModalAppears()
+        {
+           bool holder = PageObjectModel.Check_Choose_departure_modal();
+            NUnit.Framework.Assert.That(holder, Is.EqualTo(true));
         }
     }
 
